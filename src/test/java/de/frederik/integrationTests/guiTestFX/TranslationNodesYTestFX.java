12efa94de925bc4594a7953b923987420c46c009
freddyfreeloader <freddy.kuehn@gmx.net>
2022-03-24 23:53:35 +0100

    readability and redundancy TranslationNodesYTestFX

--- src/test/java/de/frederik/integrationTests/guiTestFX/TranslationNodesYTestFX.java
+++ src/test/java/de/frederik/integrationTests/guiTestFX/TranslationNodesYTestFX.java
@@ -1,7 +1,5 @@
 package de.frederik.integrationTests.guiTestFX;
 
-import javafx.application.Platform;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
@@ -10,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.time.Year;
 import java.util.Set;
 
-import static de.frederik.integrationTests.guiTestFX.utils.NodesOfFxmls.*;
+import static de.frederik.integrationTests.guiTestFX.utils.NodesOfFxmls.MENU_AGE_CHECK_BOX;
+import static de.frederik.integrationTests.guiTestFX.utils.NodesOfFxmls.MENU_VIEW;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -29,8 +28,7 @@ public class TranslationNodesYTestFX extends BaseTestFXClass {
         Set<Label> labels = helper.getLabelsFromScrollPane();
         labels.forEach(label -> {
             double translation = HBox.getMargin(label).getTop();
-            String firstWordOfLabel = label.getText().substring(0, label.getText().indexOf(' '));
-            switch (firstWordOfLabel) {
+            switch (getGivenName(label)) {
                 case "myGrandfather", "myFather", "myBrother", "myChild1" -> assertEquals(0.0, translation, "oldest persons in each generation should not be translated");
                 case "myGrandmother", "myMother", "mainPerson", "mySister", "myChild2" -> assertTrue(translation > 0.0, "persons who are younger than oldest person in generation should be translated");
             }
@@ -56,9 +54,7 @@ public class TranslationNodesYTestFX extends BaseTestFXClass {
         helper.addNewEntry(YOUNGER_PERSON, "", Year.of(2000));
         helper.addNewEntry(OLDER_PERSON, "", Year.of(1950));
 
-        Button addRelativesButtonOfMe = lookup(helper.isButtonInTableRow(ADD_RELATIVES_CSS, PERSON_WITHOUT_YEAR, "")).queryButton();
-        Platform.runLater(addRelativesButtonOfMe::fire);
-        interrupt();
+        helper.fireEditRelativesButton(PERSON_WITHOUT_YEAR, "");
 
         helper.dragAndDropToTable(YOUNGER_PERSON, SIBLINGS_TABLE);
         helper.dragAndDropToTable(OLDER_PERSON, SIBLINGS_TABLE);
@@ -70,12 +66,15 @@ public class TranslationNodesYTestFX extends BaseTestFXClass {
         Set<Label> labels = helper.getLabelsFromScrollPane();
         labels.forEach(label -> {
             double translation = HBox.getMargin(label).getTop();
-            String firstWordOfLabel = label.getText().substring(0, label.getText().indexOf(' '));
-            switch (firstWordOfLabel) {
+            switch (getGivenName(label)) {
                 case PERSON_WITHOUT_YEAR, OLDER_PERSON -> assertEquals(0.0, translation, "Person with year==null and oldest person should not be translated");
                 case YOUNGER_PERSON -> assertTrue(translation > 0.0, "persons who are younger should be translated");
             }
         });
+    }
+
+    private String getGivenName(Label label) {
+        return label.getText().substring(0, label.getText().indexOf(' '));
     }
 
     private static final String SIBLINGS_TABLE = "#siblingsTable";
